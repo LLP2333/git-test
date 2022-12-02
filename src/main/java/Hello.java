c799a0545f44a8bb198be467b15c7a6371f8b96f
llp <llp2333@qq.com>
2022-12-02 16:46:30 +0800

    first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author llp
+ * {@code @date} 2022年12月02日16:44
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
